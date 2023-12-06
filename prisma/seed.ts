2be5099ae9c1347bf10f7c3482dcaf2458b60aa9
Gil Reich <gil.reich@acronis.com>
2023-12-06 17:20:22 +0200

    compost stands will be added with seed script

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,18 +1,33 @@
-import { PrismaClient } from '@prisma/client'
+import { CompostStand, PrismaClient } from '@prisma/client'
+import { standsIdToNameMap } from '../constants/compostStands';
 const prisma = new PrismaClient()
 async function main() {
-    const liraShapira = await prisma.user.upsert({
-        where: {
-            id: process.env.LIRA_SHAPIRA_USER_ID
-        },
-        update: {},
-        create: {
-            lastName: 'SHAPIRA',
-            firstName: 'LIRA',
-            phoneNumber: '000'
-        },
-    })
-    console.log('LIRA_SHAPIRA_USER_ID: ', liraShapira.id )
+    try {
+        const compostStands: CompostStand[] = Object.entries(standsIdToNameMap).map(([key, val]) => ({
+            name: val,
+            compostStandId: parseInt(key)
+        }))
+        await prisma.compostStand.createMany({
+            data: compostStands
+        });
+
+        const liraShapira = await prisma.user.upsert({
+            where: {
+                id: process.env.LIRA_SHAPIRA_USER_ID
+            },
+            update: {},
+            create: {
+                lastName: 'SHAPIRA',
+                firstName: 'LIRA',
+                phoneNumber: '000'
+            },
+        })
+        console.log('LIRA_SHAPIRA_USER_ID: ', liraShapira.id )
+    } catch (e) {
+        console.log(e)
+    }
+
+
 }
 main()
     .then(async () => {
